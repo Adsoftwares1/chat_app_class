178ebf1b7b62e61d97361c3ecf423377f0c0c19d
Adsoftwares1 <idreeskhanapple0334@gmail.com>
2024-05-12 17:08:52 +0500

    chat function to send message

---
+++ lib/chat/model/messages_model.dart
@@ -0,0 +1,45 @@
+class Message {
+  bool deleteByReceiver;
+  bool deleteBySender;
+  String fromId;
+  String message;
+  String messageId;
+  String time;
+  String toId;
+
+  Message({
+    required this.deleteByReceiver,
+    required this.deleteBySender,
+    required this.fromId,
+    required this.message,
+    required this.messageId,
+    required this.time,
+    required this.toId,
+  });
+
+  // Factory method to create a Message object from a map
+  factory Message.fromJson(Map<String, dynamic> json) {
+    return Message(
+      deleteByReceiver: json['deleteByReceiver'] ?? false,
+      deleteBySender: json['deleteBySender'] ?? false,
+      fromId: json['fromId'] ?? '',
+      message: json['message'] ?? '',
+      messageId: json['messageId'] ?? '',
+      time: json['time'] ?? '',
+      toId: json['toId'] ?? '',
+    );
+  }
+
+  // Method to convert Message object to a map
+  Map<String, dynamic> toJson() {
+    return {
+      'deleteByReceiver': deleteByReceiver,
+      'deleteBySender': deleteBySender,
+      'fromId': fromId,
+      'message': message,
+      'messageId': messageId,
+      'time': time,
+      'toId': toId,
+    };
+  }
+}
--- lib/chat/view/inbox.dart
+++ lib/chat/view/inbox.dart
@@ -1,4 +1,6 @@
 import 'package:chat_app_for_class/chat/model/all_users_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +14,7 @@ class InboxScreen extends StatefulWidget {
 }
 
 class _InboxScreenState extends State<InboxScreen> {
+  var messageController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,14 +24,164 @@ class _InboxScreenState extends State<InboxScreen> {
         ),
         title: Text(widget.singleUserData.name),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            children: [],
-          ),
-        ],
+      body: Center(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  final isSender = true; // Replace true with your condition
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 10),
+                    child: Align(
+                      alignment: isSender
+                          ? Alignment.centerLeft
+                          : Alignment.centerRight,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 0, 43, 79),
+                          borderRadius: isSender == false
+                              ? BorderRadius.only(
+                                  topLeft: Radius.circular(20),
+                                  bottomLeft: Radius.circular(20),
+                                  bottomRight: Radius.circular(20),
+                                )
+                              : BorderRadius.only(
+                                  topRight: Radius.circular(20),
+                                  bottomLeft: Radius.circular(20),
+                                  bottomRight: Radius.circular(20),
+                                ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            "sendCancelIfRunning: isInProgress=falsecallback=ImeCallback=ImeOnBackInvokedCallback@236",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.85,
+                    child: TextField(
+                      controller: messageController,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      )),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print("Message send");
+                      sendMessage();
+                    },
+                    child: Icon(
+                      Icons.send,
+                      size: 35,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> sendMessage() async {
+    String currentDateTime = DateTime.now().microsecondsSinceEpoch.toString();
+
+    try {
+      // Check if a conversation document already exists between the two users (sender to receiver)
+      final chatQuery1 = await FirebaseFirestore.instance
+          .collection("chats")
+          .where('initiatorId',
+              isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+          .where('receiverId', isEqualTo: widget.singleUserData.userId)
+          .limit(1)
+          .get();
+
+      // Check if a conversation document already exists between the two users (receiver to sender)
+      final chatQuery2 = await FirebaseFirestore.instance
+          .collection("chats")
+          .where('initiatorId', isEqualTo: widget.singleUserData.userId)
+          .where('receiverId',
+              isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+          .limit(1)
+          .get();
+
+      // If conversation document exists, use its ID to add the message
+      if (chatQuery1.docs.isNotEmpty) {
+        final chatDocId = chatQuery1.docs.first.id;
+
+        // Add message to existing conversation document
+        FirebaseFirestore.instance
+            .collection("chats")
+            .doc(chatDocId)
+            .collection("messages")
+            .doc(currentDateTime)
+            .set({
+          'deleteByReceiver': false,
+          'deleteBySender': false,
+          'fromId': FirebaseAuth.instance.currentUser!.uid,
+          'message': messageController.text,
+          "messageId": currentDateTime,
+          "toId": widget.singleUserData.userId,
+        });
+      } else if (chatQuery2.docs.isNotEmpty) {
+        final chatDocId = chatQuery2.docs.first.id;
+
+        // Add message to existing conversation document
+        FirebaseFirestore.instance
+            .collection("chats")
+            .doc(chatDocId)
+            .collection("messages")
+            .doc(currentDateTime)
+            .set({
+          'deleteByReceiver': false,
+          'deleteBySender': false,
+          'fromId': FirebaseAuth.instance.currentUser!.uid,
+          'message': messageController.text,
+          "messageId": currentDateTime,
+          "toId": widget.singleUserData.userId,
+        });
+      } else {
+        // If conversation document doesn't exist, create a new conversation
+        FirebaseFirestore.instance.collection("chats").add({
+          'initiatorId': FirebaseAuth.instance.currentUser!.uid,
+          'receiverId': widget.singleUserData.userId,
+          'deleteBySender': false,
+          'deleteByReciever': false,
+        }).then((chatDocRef) {
+          // Add message to newly created conversation document
+          chatDocRef.collection("messages").doc(currentDateTime).set({
+            'deleteByReceiver': false,
+            'deleteBySender': false,
+            'fromId': FirebaseAuth.instance.currentUser!.uid,
+            'message': messageController.text,
+            "messageId": currentDateTime,
+            "toId": widget.singleUserData.userId,
+          });
+        });
+      }
+    } catch (e) {
+      print("Error sending message: $e");
+    }
   }
 }
